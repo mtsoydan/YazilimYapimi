3f0f9fbf8b256760cfa1ed01edc7cfb1172fddae
Berkan Sasmaz <berkansasmazz@gmail.com>
2019-05-17 00:35:42 +0300

    AzureDB configuration completed

--- IngilizceKelimeOgreniyorum/IngilizceKelimeOgreniyorum/Properties/Settings.Designer.cs
+++ IngilizceKelimeOgreniyorum/IngilizceKelimeOgreniyorum/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace IngilizceKelimeOgreniyorum.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,28 @@ namespace IngilizceKelimeOgreniyorum.Properties {
         public string KelimeOgrenmeDBConnectionString {
             get {
                 return ((string)(this["KelimeOgrenmeDBConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-9MV313N;Initial Catalog=KelimeOgrenmeDB;Integrated Security=T" +
+            "rue")]
+        public string KelimeOgrenmeDBConnectionString1 {
+            get {
+                return ((string)(this["KelimeOgrenmeDBConnectionString1"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=berkan.database.windows.net;Initial Catalog=KelimeOgrenmeAZUREDB;Pers" +
+            "ist Security Info=True;User ID=berkan;Password=7Dr87d7dr")]
+        public string KelimeOgrenmeAZUREDBConnectionString {
+            get {
+                return ((string)(this["KelimeOgrenmeAZUREDBConnectionString"]));
             }
         }
     }
